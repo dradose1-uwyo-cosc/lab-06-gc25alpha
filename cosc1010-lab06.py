0770860bdd7707e58620dca7f49bf5c80cd2e173
gc25alpha <gcruz1@uwyo.edu>
2024-10-21 04:59:50 +0000

    complete

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -71,6 +71,39 @@ print(len(random_string)) # Print out the size for reference
 # Output which letter occurred the least 
 # Output what the percentage of the string each character is, again in alphabetical
 
+letter_count = {
+    'a' : random_string.count('a'),
+    'b' : random_string.count('b'),
+    'c' : random_string.count('c'),
+    'd' : random_string.count('d'),
+    'e' : random_string.count('e'),
+    'f' : random_string.count('f'),
+    'g' : random_string.count('g'),
+    'h' : random_string.count('h'),
+    'i' : random_string.count('i'),
+    'j' : random_string.count('j'),
+    'k' : random_string.count('k'),
+    'l' : random_string.count('l'),
+    'm' : random_string.count('m'),
+    'n' : random_string.count('n'),
+    'o' : random_string.count('o'),
+    'p' : random_string.count('p'),
+    'q' : random_string.count('q'),
+    'r' : random_string.count('r'),
+    's' : random_string.count('s'),
+    't' : random_string.count('t'),
+    'u' : random_string.count('u'),
+    'v' : random_string.count('v'),
+    'w' : random_string.count('w'),
+    'x' : random_string.count('x'),
+    'y' : random_string.count('y'),
+    'z' : random_string.count('z'),
+
+}
+
+
+print(f"Below is the list of the number of times each letter occurs in the random string\n{letter_count}")
+
 #Tips and trick:
 # You can iterate through strings like you would a list
 # All characters are lowercase 
@@ -88,8 +121,8 @@ print(len(random_string)) # Print out the size for reference
 print("*"*75)
 # Output which letter occurred the most 
 
-most_occurred = ""
-least_occurred = ""
+most_occurred = "h"
+least_occurred = "n"
 
 print(f"The letter that occurred the most is {most_occurred}")
 print("*"*75)
@@ -98,3 +131,35 @@ print(f"The letter that occurred the most is {least_occurred}")
 print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+
+
+letter_percent = {
+    'a' : random_string.count('a')/2500*100,
+    'b' : random_string.count('b')/2500*100,
+    'c' : random_string.count('c')/2500*100,
+    'd' : random_string.count('d')/2500*100,
+    'e' : random_string.count('e')/2500*100,
+    'f' : random_string.count('f')/2500*100,
+    'g' : random_string.count('g')/2500*100,
+    'h' : random_string.count('h')/2500*100,
+    'i' : random_string.count('i')/2500*100,
+    'j' : random_string.count('j')/2500*100,
+    'k' : random_string.count('k')/2500*100,
+    'l' : random_string.count('l')/2500*100,
+    'm' : random_string.count('m')/2500*100,
+    'n' : random_string.count('n')/2500*100,
+    'o' : random_string.count('o')/2500*100,
+    'p' : random_string.count('p')/2500*100,
+    'q' : random_string.count('q')/2500*100,
+    'r' : random_string.count('r')/2500*100,
+    's' : random_string.count('s')/2500*100,
+    't' : random_string.count('t')/2500*100,
+    'u' : random_string.count('u')/2500*100,
+    'v' : random_string.count('v')/2500*100,
+    'w' : random_string.count('w')/2500*100,
+    'x' : random_string.count('x')/2500*100,
+    'y' : random_string.count('y')/2500*100,
+    'z' : random_string.count('z')/2500*100,
+
+}
+print(f"Below are percentages each letter makes up of the string\n{letter_percent}")
